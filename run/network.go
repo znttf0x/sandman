73358e006809f16995ec71014179888c15a3855f
znttf0x <znttf0x@users.noreply.github.com>
2026-07-05 17:38:16 -0300

    Fix network compatibility with Podman 6

--- run/network.go
+++ run/network.go
@@ -10,17 +10,16 @@ import (
 func Network(spec *specgen.SpecGenerator, containerConfig config.ContainerConfig) {
 	// Configure network namespace
 	var networkNS specgen.Namespace
-	if containerConfig.Run.Network == "" && !containerConfig.Run.Net {
-		networkNS.NSMode = specgen.None
-	} else if containerConfig.Run.Net {
-		// Backwards compatibility
-		networkNS.NSMode = specgen.Slirp
-	} else {
+	if containerConfig.Run.Network != "" {
 		var err error
 		if networkNS, _, _, err = specgen.ParseNetworkFlag([]string{containerConfig.Run.Network}); err != nil {
 			fmt.Println("Error parsing network, defaulting to none: ", err)
 			networkNS.NSMode = specgen.None
 		}
+	} else if containerConfig.Run.Net {
+		networkNS.NSMode = specgen.Pasta
+	} else {
+		networkNS.NSMode = specgen.None
 	}
 	spec.NetNS = networkNS
 }
